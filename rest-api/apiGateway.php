7d51b932e572a62450ec52dbe9455dd596cc9feb
Stefano Romanò <rumix87@gmail.com>
2015-04-07 16:30:12 +0200

    Refactored API Gateway using the new php-curl-class Added cookie support and forwarding

--- rest-api/apiGateway.php
+++ rest-api/apiGateway.php
@@ -1,58 +1,85 @@
 <?php 
 
+require_once(__DIR__ . '/../common/php/constants/global_defines.php');
+require_once(PHP_CLASSES_DIR . 'net/Curl.php');
+
+use \Curl\Curl;
+
 // Parses API URL and redirects the request to the proper interface
 
+// Get request data, including cookies
+$str_ServerName = $_SERVER['HTTP_HOST'];
 $str_RequestURL = $_SERVER['REQUEST_URI'];
 $str_RequestMethod = $_SERVER['REQUEST_METHOD'];
 
+if (isset($_COOKIE['GawainSessionID']) && isset($_COOKIE['GawainUser'])) {
+	$str_SessionID = $_COOKIE['GawainSessionID'];
+	$str_User = $_COOKIE['GawainUser'];
+} else {
+	header('Gawain-Response: Unauthorized', 0, 401);
+}
+
+
 
 // Parse the URL with regex to get entity, ID and method
-$rgx_UrlPattern ='/rest-api\/(\w+)\/?(\d*)\/?(\w*)/';
+$rgx_UrlPattern ='/(.+)\/rest-api\/(\w+)\/?(\d*)\/?(\w*)/';
 preg_match($rgx_UrlPattern, $str_RequestURL, $arr_ParsedPath); 
 
-$str_Entity = $arr_ParsedPath[1];
-$int_ID = $arr_ParsedPath[2];
-$str_Method = $arr_ParsedPath[3];
+$str_ServerURL = $arr_ParsedPath[1];
+$str_Entity = $arr_ParsedPath[2];
+$int_ID = $arr_ParsedPath[3];
+$str_Method = $arr_ParsedPath[4];
 
 
 // Get the request body if the requets method is POST and rewrite it
 if ($str_RequestMethod == 'POST') {
-	$str_RequestBody = json_decode(file_get_contents('php://input'));
+	$str_RequestBody = file_get_contents('php://input');
 	
-	$arr_RedirectBody = array(
+	$arr_RedirectFields = array(
 			'ID'		=>	$int_ID,
 			'method'	=>	$str_Method,
 			'data'		=>	$str_RequestBody
 	);
 	
-	$str_RedirectBody = json_encode($arr_RedirectBody);
-	$str_QueryString = '';
 	
 } elseif ($str_RequestMethod == 'GET') {
-	$str_RedirectBody = '';
-	$str_QueryString = '?ID=' . $int_ID;
+	$arr_RedirectFields = array('ID'	=>	$int_ID);
 }
 
 
 // Redirect the request
-$str_RedirectUrl = '/rest-api/entities/' . $str_Entity . '.php' . $str_QueryString;
+$str_RedirectUrl = $str_ServerName . $str_ServerURL . '/rest-api/entities/' . $str_Entity . '.php';
 
-$obj_CurlHandler = curl_init($str_RedirectUrl);
+$obj_Curl = new Curl();
+$obj_Curl->setCookie('GawainSessionID', $str_SessionID);
+$obj_Curl->setCookie('GawainUser', $str_User);
 
-if ($str_RequestMethod = 'POST') {
-	curl_setopt($obj_CurlHandler, CURLOPT_POST, 1);
-	curl_setopt($obj_CurlHandler, CURLOPT_POSTFIELDS, $str_RedirectBody);
+if ($str_RequestMethod == 'GET') {
+	$obj_Curl->get($str_RedirectUrl, $arr_RedirectFields);
+} elseif ($str_RequestMethod == 'POST') {
+	$obj_Curl->post($str_RedirectUrl, $arr_RedirectFields);
 }
 
-curl_setopt($obj_CurlHandler, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($obj_CurlHandler, CURLOPT_HEADER, 0);
 
-$bool_Response = curl_exec($obj_CurlHandler);
-
-if ($bool_Response) {
-	header('Gawain-Response-Code: 200', true, 200);
+if ($obj_Curl->error) {
+	echo 'Error ' . $obj_Curl->error_code . ': ' . $obj_Curl->error_message;
 } else {
-	header('Gawain-Response-Code: 404', true, 404);
+	switch ($obj_Curl->http_status_code) {
+		case 200:
+			echo $obj_Curl->response;
+			break;
+		
+		case 401:
+			header('Gawain-Response: Unauthorized', 0, 401);
+			break;
+			
+		default:
+			header('Gawain-Response: Invalid', 0, $obj_Curl->http_status_code);
+			break;
+	}
+	
+	if ($obj_Curl->http_status_code == 401 || $obj_Curl->http_status_code == 404)
+	echo $obj_Curl->response;
 }
 
 
