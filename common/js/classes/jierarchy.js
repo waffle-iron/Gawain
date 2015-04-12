cf068ff84491d0ffb008359d542a8d375e8d34c7
Stefano Romanò <rumix87@gmail.com>
2015-04-13 00:38:48 +0200

    Added Jierarchy class to manage library and class dependencies. Added Jierarchy JSON configuration file

---
+++ common/js/classes/jierarchy.js
@@ -0,0 +1,19 @@
+function jierarchy(str_DependencyFile) {
+	
+	// The file containing the dependencies of all the available libraries
+	this.depSource = str_DependencyFile;
+	
+	// The object containing the dependency tree
+	this.depTree = {};
+	
+}
+
+
+jierarchy.prototype.calculateDependencies = function() {
+	
+}
+
+
+jierarchy.prototype.load = function(str_LibraryName) {
+	
+}
