c02b6cfc52828dc69a6c3560145d86bf17d60a41
Stefano Romanò <rumix87@gmail.com>
2015-04-14 00:02:19 +0200

    Almost finished Jierarchy JavaScript class to easily handle JavaScript and CSS dependencies. Started compiling dependencies file. Added .htaccess Directory Index command to override server defaults.

--- common/js/classes/jierarchy.js
+++ common/js/classes/jierarchy.js
@@ -1,19 +1,83 @@
-function jierarchy(str_DependencyFile) {
+function jierarchy(str_DepFilePath) {
 	
 	// The file containing the dependencies of all the available libraries
-	this.depSource = str_DependencyFile;
+	this.depSource = {};
 	
-	// The object containing the dependency tree
-	this.depTree = {};
+	// The array of JavaScript paths
+	this.JsPaths = [];
+	
+	// The array of JavaScript paths
+	this.CssPaths = [];
+	
+
+	
+	// Get the JSON dependencies file
+	var obj_XHR = new XMLHttpRequest();
+	obj_XHR.overrideMimeType('application/json');
+	
+	obj_XHR.open('GET', str_DepFilePath, false);
+	obj_XHR.send();
+	this.depSource = JSON.parse(obj_XHR.responseText);
 	
 }
 
 
-jierarchy.prototype.calculateDependencies = function() {
+
+
+jierarchy.prototype.load = function(arr_LibraryNames) {
+	var self = this;
 	
+	var str_LibName;
+	var int_LibLength = arr_LibraryNames.length;
+	var int_LibCounter;
+	var obj_CurrentNode;
+	
+	var int_PathCounter;
+	
+	for (int_LibCounter = 0; int_LibCounter < int_LibLength; int_LibCounter++) {
+		str_LibName = arr_LibraryNames[int_LibCounter];
+		obj_CurrentNode = this.depSource[str_LibName];
+		
+		
+		// Recurse function
+		if (obj_CurrentNode.dependencies.length > 0) {
+			self.load(obj_CurrentNode.dependencies);
+		}
+		
+		
+		if (obj_CurrentNode.path.js.length > 0) {
+			for (int_PathCounter = 0; int_PathCounter < obj_CurrentNode.path.js.length; int_PathCounter++) {
+				if (obj_CurrentNode.path.js[int_PathCounter] != '') {
+					this.JsPaths.push(obj_CurrentNode.path.js[int_PathCounter]);
+				}
+			}
+		}
+		
+		if (obj_CurrentNode.path.css.length > 0) {
+			for (int_PathCounter = 0; int_PathCounter < obj_CurrentNode.path.css.length; int_PathCounter++) {
+				if (obj_CurrentNode.path.css[int_PathCounter] != '') {
+					this.CssPaths.push(obj_CurrentNode.path.css[int_PathCounter]);
+				}
+			}
+		}
+		
+	}
+	
+	this.JsPaths = this.JsPaths.getUnique();
+	this.CssPaths = this.CssPaths.getUnique();
 }
 
 
-jierarchy.prototype.load = function(str_LibraryName) {
-	
-}
+
+// Adding getUnique function to array prototype to get unique values
+Array.prototype.getUnique = function(){
+	   var u = {}, a = [];
+	   for(var i = 0, l = this.length; i < l; ++i){
+	      if(u.hasOwnProperty(this[i])) {
+	         continue;
+	      }
+	      a.push(this[i]);
+	      u[this[i]] = 1;
+	   }
+	   return a;
+	}
---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+
+<html>
+<head>
+
+<script src="common/js/classes/jierarchy.js" type="text/javascript"></script>
+
+<script>
+
+var obj_Jierarchy = new jierarchy("common/js/dependencies/dependencies.json");
+obj_Jierarchy.load(['jQuery', 'bootstrap']);
+
+</script>
+
+</head>
+
+
+</html>
