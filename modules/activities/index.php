34354e5ec440757bf74575995c132a3c428770eb
Stefano Romanò <rumix87@gmail.com>
2015-06-16 19:45:49 +0200

    Revert "Started integrating Slim Framework into existing application"
    This reverts commit 24d03e07a3450e0e562cc5fb162d62143b7802b3.

--- modules/activities/index.php
+++ modules/activities/index.php
@@ -5,10 +5,7 @@ require_once(PHP_CLASSES_DIR . 'net/Jierarchy.php');
 require_once(PHP_CLASSES_DIR . 'rendering/HtmlRenderer.php');
 require_once(PHP_CLASSES_DIR . 'auths/UserAuthManager.php');
 require_once(PHP_CLASSES_DIR . 'entities/Activity.php');
-require_once(PHP_VENDOR_DIR . 'Slim/Slim.php');
 require_once(TEMPLATES_DIR . 'html/Default/page_navbar.php');
-
-\Slim\Slim::registerAutoloader();
 
 
 // Check permission for the current page
@@ -16,84 +13,41 @@ $obj_AuthManager = new UserAuthManager($_SERVER['REMOTE_ADDR']);
 $obj_AuthManager->checkPermissions('activities', TRUE);
 
 
-// Creates Slim app
-$app = new Slim\Slim();
+
+// Loads the navbar data
+$arr_Data = common\page_navbar\data_source();
 
 
 
-// Global activity view
-$app->get('/', function() {
-
-	// Loads the navbar data
-	$arr_Data = common\page_navbar\data_source();
-
-	// Calculates and prepares the page library dependencies
-	$obj_Jierarchy = new Jierarchy(JS_DIR . 'dependencies/dependencies.json');
-	$arr_Data['page_dependencies'] = $obj_Jierarchy->load(array(
-		                                                      'jQuery',
-		                                                      'bootstrap',
-		                                                      'bootstrap-cerulean-theme',
-		                                                      'gawain-style-settings',
-		                                                      'gawain-button-bindings',
-		                                                      'font-awesome',
-		                                                      'jquery-treegrid',
-		                                                      'gawain-treegrid-onload',
-		                                                      "dhtmlx-Gantt"
-	                                                      ));
-
-	// Gets activity data, skipping referentials for parent activity
-	$obj_Activity = new Activity($_COOKIE['GawainSessionID']);
-	$arr_Data['activities'] = $obj_Activity->read(NULL, array('activityParentID'));
-	$arr_Data['module_label'] = $obj_Activity->entityLabel;
-	$arr_Data['activity_types'] = $obj_Activity->getActivityTypes();
+// Calculates and prepares the page library dependencies
+$obj_Jierarchy = new Jierarchy(JS_DIR . 'dependencies/dependencies.json');
+$arr_Data['page_dependencies'] = $obj_Jierarchy->load(array(
+	                     'jQuery',
+	                     'bootstrap',
+	                     'bootstrap-cerulean-theme',
+	                     'gawain-style-settings',
+	                     'gawain-button-bindings',
+	                     'font-awesome',
+	                     'jquery-treegrid',
+                         'gawain-treegrid-onload',
+	                     "dhtmlx-Gantt"
+                     ));
 
 
-	// Renders the webpage
-	$obj_Renderer = new HtmlRenderer();
-	$obj_Renderer->addTemplatePath(__DIR__ . '/templates/html/Default');
-	$obj_Renderer->importData($arr_Data);
-	$obj_Renderer->setStyle('Default');
-	$obj_Renderer->setTemplate('webpage_all.twig');
-	echo $obj_Renderer->render();
-
-});
+// Gets activity data, skipping referentials for parent activity
+$obj_Activity = new Activity($_COOKIE['GawainSessionID']);
+$arr_Data['activities'] = $obj_Activity->read(NULL, array('activityParentID'));
+$arr_Data['module_label'] = $obj_Activity->entityLabel;
+$arr_Data['activity_types'] = $obj_Activity->getActivityTypes();
 
 
-// Details of activity, read mode
-$app->get('/:id', function($id) {   // FIXME: move the app declaration in main index.php
+// Renders the webpage
+$obj_Renderer = new HtmlRenderer();
+$obj_Renderer->addTemplatePath(__DIR__ . '/templates/html/Default');
+$obj_Renderer->importData($arr_Data);
+$obj_Renderer->setStyle('Default');
+$obj_Renderer->setTemplate('webpage_all.twig');
 
-	// Loads the navbar data
-	$arr_Data = common\page_navbar\data_source();
+//var_dump($arr_Data['activities']);
 
-	// Calculates and prepares the page library dependencies
-	$obj_Jierarchy = new Jierarchy(JS_DIR . 'dependencies/dependencies.json');
-	$arr_Data['page_dependencies'] = $obj_Jierarchy->load(array(
-		                                                      'jQuery',
-		                                                      'bootstrap',
-		                                                      'bootstrap-cerulean-theme',
-		                                                      'gawain-style-settings',
-		                                                      'gawain-button-bindings',
-		                                                      'font-awesome',
-		                                                      'jquery-treegrid',
-		                                                      'gawain-treegrid-onload',
-		                                                      "dhtmlx-Gantt"
-	                                                      ));
-
-	// Gets activity data, skipping referentials for parent activity
-	$obj_Activity = new Activity($_COOKIE['GawainSessionID']);
-	$arr_Data['activities'] = $obj_Activity->read($id);
-	$arr_Data['module_label'] = $obj_Activity->entityLabel;
-
-
-	// Renders the webpage
-	$obj_Renderer = new HtmlRenderer();
-	$obj_Renderer->addTemplatePath(__DIR__ . '/templates/html/Default');
-	$obj_Renderer->importData($arr_Data);
-	$obj_Renderer->setStyle('Default');
-	$obj_Renderer->setTemplate('webpage_details.twig');
-	echo $obj_Renderer->render();
-
-});
-
-
-$app->run();
+echo $obj_Renderer->render();
