e5835947bc7306f56bf692ecd1d1ed8b919c4fb0
Stefano Romanò <rumix87@gmail.com>
2015-07-01 23:55:13 +0200

    Start creating the single activity page

--- modules/activities/controller.php
+++ modules/activities/controller.php
@@ -4,47 +4,94 @@ require_once(COMMON_DIR . 'templates/html/Default/page_navbar.php');
 require_once(PHP_CLASSES_DIR . 'entities/Activity.php');
 
 
-$app->get('/activities', function () use($app, $loader, $obj_Jierarchy) {
-
-	// Get session ID
-	$str_SessionID = $app->getCookie('GawainSessionID');
+// Get session ID
+$str_SessionID = $app->getCookie('GawainSessionID');
 
 
-	// Page dependencies
-	$arr_PageDependencies = $obj_Jierarchy->load(array(
-		                     'jQuery',
-		                     'bootstrap',
-		                     'bootstrap-cerulean-theme',
-		                     'gawain-style-settings',
-		                     'gawain-button-bindings',
-		                     'font-awesome',
-		                     'jquery-treegrid',
-		                     'gawain-treegrid-onload',
-		                     "dhtmlx-Gantt"
-	                     ));
+$app->group('/activities', function () use($app, $loader, $obj_Jierarchy, $str_SessionID) {
+
+	$app->get('/', function () use($app, $loader, $obj_Jierarchy, $str_SessionID) {
+
+		// Page dependencies
+		$arr_PageDependencies = $obj_Jierarchy->load(array(
+			                     'jQuery',
+			                     'bootstrap',
+			                     'bootstrap-cerulean-theme',
+			                     'gawain-style-settings',
+			                     'gawain-button-bindings',
+			                     'font-awesome',
+			                     'jquery-treegrid',
+			                     'gawain-treegrid-onload',
+			                     "dhtmlx-Gantt"
+		                     ));
 
 
-	// Navbar data declaration
-	$arr_NavbarData = common\page_navbar\data_source($str_SessionID, 'activities');
+		// Navbar data declaration
+		$arr_NavbarData = common\page_navbar\data_source($str_SessionID, 'activities');
 
 
-	//Activity object declaration and usage
-	$obj_Activity = new Activity($str_SessionID);
-	$arr_Activities = $obj_Activity->read(NULL, array('activityParentID'));
-	$str_ModuleLabel = $obj_Activity->entityLabel;
-	$arr_ActivityTypes = $obj_Activity->getActivityTypes();
+		//Activity object declaration and usage
+		$obj_Activity = new Activity($str_SessionID);
+		$arr_Activities = $obj_Activity->read(NULL, array('activityParentID'));
+		$str_ModuleLabel = $obj_Activity->entityLabel;
+		$arr_ActivityTypes = $obj_Activity->getActivityTypes();
 
 
-	// Prepare the view
-	$app->view()->set('page_dependencies', $arr_PageDependencies);
-	$app->view()->set('navbar_data', $arr_NavbarData);
-	$app->view()->set('activities', $arr_Activities);
-	$app->view()->set('module_label', $str_ModuleLabel);
-	$app->view()->set('activity_types', $arr_ActivityTypes);
+		// Prepare the view
+		$app->view()->set('page_dependencies', $arr_PageDependencies);
+		$app->view()->set('navbar_data', $arr_NavbarData);
+		$app->view()->set('activities', $arr_Activities);
+		$app->view()->set('module_label', $str_ModuleLabel);
+		$app->view()->set('activity_types', $arr_ActivityTypes);
 
 
-	// Renders the page
-	$loader->addPath(MODULES_DIR . 'activities/templates/html/Default');
-	$app->render('webpage_all.twig');
+		// Renders the page
+		$loader->addPath(MODULES_DIR . 'activities/templates/html/Default');
+		$app->render('webpage_all.twig');
 
-})->name('activities');
+	})->name('activities');
+
+
+
+	$app->get('/:activityID', function ($activityID) use ($app, $loader, $obj_Jierarchy, $str_SessionID) {
+
+		// Page dependencies
+		$arr_PageDependencies = $obj_Jierarchy->load(array(
+			                                             'jQuery',
+			                                             'bootstrap',
+			                                             'bootstrap-cerulean-theme',
+			                                             'gawain-style-settings',
+			                                             'gawain-button-bindings',
+			                                             'font-awesome',
+			                                             "dhtmlx-Gantt"
+		                                             ));
+
+
+		// Navbar data declaration
+		$arr_NavbarData = common\page_navbar\data_source($str_SessionID, 'activities');
+
+
+		//Activity object declaration and usage
+		$obj_Activity = new Activity($str_SessionID);
+		$arr_ActivityData = $obj_Activity->read($activityID);
+		$str_ModuleLabel = $obj_Activity->entityLabel;
+		$str_ItemLabel = $obj_Activity->entityItemLabel;
+
+
+		// Prepare the view
+		$app->view()->set('page_dependencies', $arr_PageDependencies);
+		$app->view()->set('navbar_data', $arr_NavbarData);
+		$app->view()->set('activity_ID', $activityID);
+		$app->view()->set('activity_data', $arr_ActivityData['data'][$activityID]);
+		$app->view()->set('activity_fields', $arr_ActivityData['fields']);
+		$app->view()->set('module_label', $str_ModuleLabel);
+		$app->view()->set('module_item_label', $str_ItemLabel);
+
+
+		// Renders the page
+		$loader->addPath(MODULES_DIR . 'activities/templates/html/Default');
+		$app->render('webpage_single_show.twig');
+
+	});
+
+});
