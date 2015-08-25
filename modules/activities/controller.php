9e8019212d9cc0693e4f77287ce974b6253d2df6
Stefano Romanò <rumix87@gmail.com>
2015-08-26 01:03:35 +0200

    Added activity creation/editing controller and rendering template Refactored other controllers to include entity data and fields info decoupling (see previous commit)

--- modules/activities/controller.php
+++ modules/activities/controller.php
@@ -33,6 +33,7 @@ $app->group('/activities', function () use($app, $loader, $obj_Jierarchy, $str_S
 		//Activity object declaration and usage
 		$obj_Activity = new Activity($str_SessionID);
 		$arr_Activities = $obj_Activity->read(NULL, array('activityParentID'));
+		$arr_ActivityFields = $obj_Activity->getFieldsData();
 		$str_ModuleLabel = $obj_Activity->entityLabel;
 		$arr_ActivityTypes = $obj_Activity->getActivityTypes();
 
@@ -40,7 +41,8 @@ $app->group('/activities', function () use($app, $loader, $obj_Jierarchy, $str_S
 		// Prepare the view
 		$app->view()->set('page_dependencies', $arr_PageDependencies);
 		$app->view()->set('navbar_data', $arr_NavbarData);
-		$app->view()->set('activities', $arr_Activities);
+		$app->view()->set('activities_data', $arr_Activities);
+		$app->view()->set('activities_fields', $arr_ActivityFields);
 		$app->view()->set('module_label', $str_ModuleLabel);
 		$app->view()->set('activity_types', $arr_ActivityTypes);
 
@@ -68,12 +70,13 @@ $app->group('/activities', function () use($app, $loader, $obj_Jierarchy, $str_S
 
 
 		// Navbar data declaration
-		$arr_NavbarData = common\page_navbar\data_source($str_SessionID, 'activities');
+		$arr_NavbarData = common\page_navbar\data_source($str_SessionID, NULL);
 
 
 		//Activity object declaration and usage
 		$obj_Activity = new Activity($str_SessionID);
 		$arr_ActivityData = $obj_Activity->read($activityID);
+		$arr_ActivityFields = $obj_Activity->getFieldsData();
 		$str_ModuleLabel = $obj_Activity->entityLabel;
 		$str_ItemLabel = $obj_Activity->entityItemLabel;
 
@@ -82,8 +85,8 @@ $app->group('/activities', function () use($app, $loader, $obj_Jierarchy, $str_S
 		$app->view()->set('page_dependencies', $arr_PageDependencies);
 		$app->view()->set('navbar_data', $arr_NavbarData);
 		$app->view()->set('activity_ID', $activityID);
-		$app->view()->set('activity_data', $arr_ActivityData['data'][$activityID]);
-		$app->view()->set('activity_fields', $arr_ActivityData['fields']);
+		$app->view()->set('activity_data', $arr_ActivityData[$activityID]);
+		$app->view()->set('activity_fields', $arr_ActivityFields);
 		$app->view()->set('module_label', $str_ModuleLabel);
 		$app->view()->set('module_item_label', $str_ItemLabel);
 
@@ -94,6 +97,72 @@ $app->group('/activities', function () use($app, $loader, $obj_Jierarchy, $str_S
 		$loader->addPath(MODULES_DIR . 'activities/templates/html/Default');
 		$app->render('webpage_single_show.twig');
 
-	})->name('activity_details_show');
+	})->conditions(array('activityID' => '\d+'))->name('activity_show');
+
+
+
+
+	$app->get('/new', function () use ($app, $loader, $obj_Jierarchy, $str_SessionID) {
+
+		// Page dependencies
+		$arr_PageDependencies = $obj_Jierarchy->load(array(
+			                                             'jQuery',
+			                                             'bootstrap',
+			                                             'bootstrap-cerulean-theme',
+			                                             'gawain-style-settings',
+			                                             'gawain-button-bindings',
+			                                             'font-awesome'
+		                                             ));
+
+		// Navbar data declaration
+		$arr_NavbarData = common\page_navbar\data_source($str_SessionID, NULL);
+
+		// If the request comes from a "Clone", then copy data from selected activity
+		// If the request is a simple "New", no data is set as default value
+		$int_ClonedActivityID = $app->request->get('cloneFrom');
+
+		$obj_Activity = new Activity($str_SessionID);
+		$arr_ActivityData = is_null($int_ClonedActivityID) ? NULL : $obj_Activity->read($int_ClonedActivityID);
+		$arr_ActivityFields = $obj_Activity->getFieldsData();
+		$str_DomainDependencyColumn = $obj_Activity->getDomainDependencyColumn();
+		$str_MainID = $obj_Activity->mainID;
+		$str_ModuleLabel = $obj_Activity->entityLabel;
+		$str_ItemLabel = $obj_Activity->entityItemLabel;
+
+
+		// If the type is specified in the request, the field is automatically populated, else it is empty
+		$int_ActivityTypeID = $app->request->get('activityType');
+
+
+		// Prepare the view
+		$app->view()->set('page_dependencies', $arr_PageDependencies);
+		$app->view()->set('navbar_data', $arr_NavbarData);
+		$app->view()->set('activity_data', is_null($int_ClonedActivityID) ? NULL : $arr_ActivityData[$int_ClonedActivityID]);
+		$app->view()->set('activity_fields', $arr_ActivityFields);
+		$app->view()->set('module_label', $str_ModuleLabel);
+		$app->view()->set('module_item_label', $str_ItemLabel);
+		$app->view()->set('activity_type_ID', $int_ActivityTypeID);
+		$app->view()->set('activity_domain_dependency_column', $str_DomainDependencyColumn);
+		$app->view()->set('activity_main_id', $str_MainID);
+
+
+
+		// Action switch to define the view once and use it for edit and creation
+		$app->view()->set('page_action', 'new');
+
+
+		// Renders the page
+		$loader->addPath(MODULES_DIR . 'activities/templates/html/Default');
+		$app->render('webpage_single_new_edit.twig');
+
+	})->name('activity_new');
+
+
+
+	$app->post('/new/save', function () use ($app) {
+
+		$app->redirect($app->urlFor('activities'));
+
+	})->name('activity_new_save');
 
 });
