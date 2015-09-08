1dee21f835450792f1dfdc7f6349789dd9fa6196
Stefano Romanò <rumix87@gmail.com>
2015-09-08 22:44:52 +0200

    Added method to calculate estimated effort for the given activity

--- common/php/classes/entities/Activity.php
+++ common/php/classes/entities/Activity.php
@@ -2,6 +2,7 @@
 
 require_once(__DIR__ . '/../../constants/global_defines.php');
 require_once(PHP_ABSTRACTS_DIR . 'entities/Entity.php');
+require_once(PHP_FUNCTIONS_DIR . 'string_functions.php');
 
 class Activity extends Entity {
 
@@ -48,6 +49,93 @@ class Activity extends Entity {
 		return $arr_Output;
 
 	}
+
+
+	/** Calculates the estimated effort for the given activity.
+	 *  If the effort is manually set, it simply returns the value.
+	 *  If the effort is set to be automatically calculated, the value is retrieved as the recursive sum of the descendant effort
+	 *
+	 * @param int $int_ActivityID The activity ID for which the effort has to be calculated
+	 * @return float
+	 */
+	public function getEstimatedEffort($int_ActivityID) {
+
+		// First get activities and saved data
+		$str_Query = '
+			select
+				activities.activityID,
+				activities.activityParentID,
+				activities.activityEstimatedEffortHours as effort,
+				activities.activityIsEstimatedEffortHoursAuto as isAuto
+			from activities
+			where activities.activityCustomerID = ?
+				and activities.activityID = ?
+		';
+
+		$obj_Resultset = $this->dbHandler->executePrepared($str_Query,
+		                                                   array(
+			                                                   array($this->domainID => 'i'),
+			                                                   array($int_ActivityID => 'i')
+		                                                   ));
+
+
+		$int_Result = NULL;
+
+
+		if (!(boolean) $obj_Resultset[0]['isAuto']) {
+
+			// If a value is present and the Aut flag si false, simply return the overridden value
+			$int_Result = is_null($obj_Resultset[0]['effort']) ? 0 : floatval($obj_Resultset[0]['effort']);
+
+		} else {
+
+			// If the Auto flag is set to True, the effort is recursively calculated from the child tasks
+			$str_Query = '
+				select
+					activities.activityID,
+					activities.activityParentID,
+					activities.activityEstimatedEffortHours as effort,
+					activities.activityIsEstimatedEffortHoursAuto as isAuto
+				from activities
+				where activities.activityCustomerID = ?
+					and activities.activityParentID = ?
+			';
+
+			$obj_ResultsetChild = $this->dbHandler->executePrepared($str_Query,
+			                                                   array(
+				                                                   array($this->domainID => 'i'),
+				                                                   array($int_ActivityID => 'i')
+			                                                   ));
+
+
+			if (count($obj_ResultsetChild) == 0) {
+
+				// If no child is found, return zero
+				$int_Result = 0;
+
+			} else {
+
+				// If children are found, for each of them repeat the procedure and get the effort
+				$arr_ChildEffort = array();
+
+				foreach ($obj_ResultsetChild as $arr_Datarow) {
+					$arr_ChildEffort[] = $this->getEstimatedEffort($arr_Datarow['activityID']);
+				}
+
+				// The final effort is the sum of the children's effort
+				$int_Result = array_sum($arr_ChildEffort);
+
+			}
+
+		}
+
+
+		return $int_Result;
+
+	}
+
+
+
 	
 
 }
