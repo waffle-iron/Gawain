6d886f58ecf58c26b4672a5ab3a6bd49092abcf6
Stefano Romanò <rumix87@gmail.com>
2016-01-07 17:39:05 +0100

    Added "New Timeslot" page and controller function

--- modules/timeslots/controller.php
+++ modules/timeslots/controller.php
@@ -83,13 +83,110 @@ $app->group('/timeslots', function () use ($app, $loader, $obj_Jierarchy, $str_S
 
 	$app->get('/new', function () use ($app, $loader, $obj_Jierarchy, $str_SessionID) {
 
+		// Page dependencies
+		$arr_PageDependencies = $obj_Jierarchy->load(array(
+			                                             'jQuery',
+			                                             'bootstrap',
+			                                             'bootstrap-cerulean-theme',
+			                                             'gawain-style-settings',
+			                                             'gawain-button-bindings',
+			                                             'font-awesome'
+		                                             ));
+
+		// Navbar data declaration
+		$arr_NavbarData = common\page_navbar\data_source($str_SessionID, NULL);
+
+		// If the request comes from a "Clone", then copy data from selected timeslot
+		// If the request is a simple "New", no data is set as default value
+		$int_ClonedTimeslotID = $app->request->get('cloneFrom');
+
+		$obj_Timeslot = new Timeslot($str_SessionID);
+		$arr_TimeslotData = is_null($int_ClonedTimeslotID) ? NULL : $obj_Timeslot->read($int_ClonedTimeslotID);
+		$arr_TimeslotFields = $obj_Timeslot->getFieldsData();
+		$str_DomainDependencyColumn = $obj_Timeslot->getDomainDependencyColumn();
+		$str_MainID = $obj_Timeslot->getPrimaryKey();
+		$str_ModuleLabel = $obj_Timeslot->getLabel();
+		$str_ItemLabel = $obj_Timeslot->getItemLabel();
+
+
+		// Prepare the view
+		$app->view()->set('page_dependencies', $arr_PageDependencies);
+		$app->view()->set('navbar_data', $arr_NavbarData);
+		$app->view()->set('data', is_null($int_ClonedTimeslotID) ? NULL : $arr_TimeslotData[$int_ClonedTimeslotID]);
+		$app->view()->set('fields', $arr_TimeslotFields);
+		$app->view()->set('module_label', $str_ModuleLabel);
+		$app->view()->set('module_item_label', $str_ItemLabel);
+		$app->view()->set('domain_dependency_column', $str_DomainDependencyColumn);
+		$app->view()->set('main_id', $str_MainID);
+
+
+
+		// Action switch to define the view once and use it for edit and creation
+		$app->view()->set('page_action', 'new');
+		$app->view()->set('entity_new_save_link_name', 'timeslot_new_save');
+		$app->view()->set('entity_edit_save_link_name', 'timeslot_edit_save');
+
+
+		// Renders the page
+		$loader->addPath(COMMON_DIR . 'templates/html/Default');
+		$app->render('single_entity_new_edit.twig');
+
 	})->name('timeslot_new');
+
+
+
+	$app->post('/new/save', function () use ($app, $str_SessionID) {
+
+		// Get all the POST variables
+		$arr_PostVariables = $app->request->post();
+
+		// Get all activity fields info
+		$obj_Timeslot = new Timeslot($str_SessionID);
+		$arr_TimeslotFields = $obj_Timeslot->getFieldsData();
+
+		// Iterate over array to nullify empty strings
+		foreach (array_keys($arr_PostVariables) as $str_PostKey) {
+			if ($arr_PostVariables[$str_PostKey] == '') {
+				if ($arr_TimeslotFields[$str_PostKey]['type'] == 'BOOL') {
+					$arr_PostVariables[$str_PostKey] = 0;
+				} else {
+					$arr_PostVariables[$str_PostKey] = NULL;
+				}
+			}
+		}
+
+		// Save activity data
+		$obj_Timeslot->insert($arr_PostVariables);
+		$app->redirect($app->urlFor('timeslots'));
+
+	})->name('timeslot_new_save');
 
 
 
 	$app->get('/:timeslotID/edit', function ($timeslotID) use ($app, $loader, $obj_Jierarchy, $str_SessionID) {
 
 	})->name('timeslot_edit');
+
+
+
+	$app->post('/:timeslotID/save', function ($timeslotID) use ($app, $str_SessionID) {
+
+		// Get all the POST variables
+		$arr_PostVariables = $app->request->post();
+		$obj_Timeslot = new Timeslot($str_SessionID);
+
+		// Iterate over array to unset empty strings
+		foreach (array_keys($arr_PostVariables) as $str_PostKey) {
+			if ($arr_PostVariables[$str_PostKey] == '') {
+				unset($arr_PostVariables[$str_PostKey]);
+			}
+		}
+
+		// Save activity data
+		$obj_Timeslot->update($timeslotID, $arr_PostVariables);
+		$app->redirect($app->urlFor('timeslots'));
+
+	})->conditions(array('timeslotID' => '\d+'))->name('timeslot_edit_save');
 
 
 
